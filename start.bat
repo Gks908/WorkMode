7bc89ec3fa1c9e24955402fe1a65fa3741e6d766
Gks908 <139458440+Gks908@users.noreply.github.com>
2023-07-14 23:02:48 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,4 @@
 @echo off
 powershell -Command "Invoke-WebRequest https://github.com/gravitational/teleconsole/releases/latest/download/teleconsole-windows-amd64.exe -OutFile teleconsole.exe"
 
+teleconsole --port 3389

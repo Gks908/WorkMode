c9e56254b004eef13a86d1abd40665b6c660841d
Gks908 <139458440+Gks908@users.noreply.github.com>
2023-07-14 21:56:47 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,9 +2,11 @@
 start "" "C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe"
 timeout 5
 echo Retrieving AnyDesk ID...
-"C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe" --get-id > anydesk_id.txt
+"C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe" --get-id
+
+
 echo disalardp | anydesk --set-password
 echo AnyDesk ID: 
-type anydesk_id.txt
+
 
 

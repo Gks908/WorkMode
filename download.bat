5a7e4e721c9285252c67c5576393f8836a2ac852
Gks908 <139458440+Gks908@users.noreply.github.com>
2023-07-14 21:20:05 +0530

    Update download.bat

--- download.bat
+++ download.bat
@@ -1,18 +1,3 @@
 @echo off
 powershell -Command "choco install anydesk -y"
-powershell -Command "choco install qbittorrent -y"
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Gks908/WorkMode/main/start.bat -OutFile start.bat"
-pip install pyautogui
-
-curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
-curl -s -L -o C:\Users\Public\Desktop\YTC.zip https://drive.google.com/u/0/uc?id=1S7doIyi73_dUGQUrMbM0nxJEai3JaKiO
-curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
-C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
-del C:\Users\Public\Desktop\Telegram.exe
-C:\Users\Public\Desktop\Winrar.exe /S
-del C:\Users\Public\Desktop\Winrar.exe
-del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
-del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > errormsg.txt 2>&1
-set password=@#Disala123456
-powershell -Command "Set-LocalUser -Name 'runneradmin' -Password (ConvertTo-SecureString -AsPlainText '%password%' -Force)"

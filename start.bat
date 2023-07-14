22ca2f4e668ab140da2855c2d86ef5a09961e4c9
Gks908 <139458440+Gks908@users.noreply.github.com>
2023-07-14 21:52:10 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,30 +1,10 @@
 @echo off
-
-:START_ANYDESK
-anydesk --start
-ping 127.0.0.1 -n 4 > nul
-
-for /f "delims=" %%i in ('"C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe" --get-id') do (
-    set ID=%%i
-)
-
-if "%ID%"=="" (
-    taskkill /f /im anydesk.exe > nul
-    goto START_ANYDESK
-)
-
-if "%ID%"=="0" (
-    taskkill /f /im anydesk.exe > nul
-    goto START_ANYDESK
-)
-
-
+start "" "C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe"
+timeout 5
+echo Retrieving AnyDesk ID...
+"C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe" --get-id > anydesk_id.txt
 echo disalardp | anydesk --set-password
+echo AnyDesk ID: 
+type anydesk_id.txt
 
 
-
-echo AnyDesk ID is: %ID%
-
-echo AnyDesk Password: disalardp
-
-echo You can login now!

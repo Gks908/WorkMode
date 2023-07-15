653abc592021ad6f1790dddd93c30b69ef3b423b
Gks908 <139458440+Gks908@users.noreply.github.com>
2023-07-15 09:44:41 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -7,9 +7,9 @@ choco install teamviewer -y
 
 cd "C:\Program Files\TeamViewer"
 TeamViewer_Setup_x64.exe
-TeamViewer_Setup_x64.exe --passwd [disalardp]
+TeamViewer.exe --passwd [disalardp]
 
 
-TeamViewer_Setup_x64.exe --info print version-id
+TeamViewer.exe --info print version-id
 
 

6dca2c01bf421d3e4840f6a2f1cf2a3d0a74340b
Gks908 <139458440+Gks908@users.noreply.github.com>
2023-07-15 22:45:32 +0530

    Delete download.bat

--- download.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -Command "choco install anydesk -y"
-powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Gks908/WorkMode/main/start.bat -OutFile start.bat"

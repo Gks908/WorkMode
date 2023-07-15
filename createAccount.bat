8b002e47a0b2a3d3810a0e5e0f851648d0889900
Gks908 <139458440+Gks908@users.noreply.github.com>
2023-07-15 22:45:11 +0530

    Delete createAccount.bat

--- createAccount.bat
+++
@@ -1,2 +0,0 @@
-powershell -Command "New-LocalUser -Name "Gurpreet" -Password (ConvertTo-SecureString -String "Gurpreet@1414#&" -AsPlainText -Force) -AccountNeverExpires -UserMayNotChangePassword"
-

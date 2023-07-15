37087af8c2300e39c49b1b2d786a3f1e62d8e5e7
Gks908 <139458440+Gks908@users.noreply.github.com>
2023-07-15 10:15:34 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,15 +1,6 @@
 @echo off
-
-
-
-choco install teamviewer -y
-
-
-cd "C:\Program Files\TeamViewer"
-TeamViewer_Setup_x64.exe
-TeamViewer.exe --passwd [disalardp]
-
-
-TeamViewer.exe --info print version-id
-
-
+curl -O https://download.anydesk.com/AnyDesk.exe
+AnyDesk.exe --install  “C:\Program Files (x86)\AnyDesk” --start-with-win --create-desktop-icon
+for /f "delims=" %%i in ('"C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --get-id') do set ID=%%i 
+echo AnyDesk ID is: %ID%
+pause

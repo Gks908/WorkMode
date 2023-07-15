4bf0c27be399c1601c1b8d06974597723f7408a5
Gks908 <139458440+Gks908@users.noreply.github.com>
2023-07-15 13:23:25 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 @echo off
-curl -O https://download.anydesk.com/AnyDesk.exe
-AnyDesk.exe --install  “C:\Program Files (x86)\AnyDesk” --start-with-win --create-desktop-icon
-for /f "delims=" %%i in ('"C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --get-id') do set ID=%%i 
+powershell -Command "choco install anydesk -y"
+anydesk --start
+echo disalardp | anydesk --set-password
+for /f "delims=" %%i in ('"C:\ProgramData\chocolatey\lib\anydesk.portable\tools\AnyDesk.exe" --get-id') do set ID=%%i 
 echo AnyDesk ID is: %ID%
 pause
